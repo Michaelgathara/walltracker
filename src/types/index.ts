5847e453f4711164a0e21279b05916ca07466801
Michaelgathara <mikegtrm@gmail.com>
2026-05-17 21:44:03 -0500

    refactor: move shared domain types

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export type { Aircraft, AircraftFeed, AircraftTrackPoint } from "./aircraft";
+export type { AnimalObservation, AnimalObservationFeed } from "./animals";
+export type { Coordinates } from "./geo";
+export type { SunPhase, SunState } from "./sun";
